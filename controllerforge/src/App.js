7326b4801561356bbbf8891a27b8bacd583139c8
Jack Burnett <jackjburnett@gmail.com>
2024-06-28 20:06:59 +0100

    Added LexendDeca

--- controllerforge/src/App.js
+++ controllerforge/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './App.css';
 import STLViewer from './STLViewer';
 import { ThemeProvider, ThemeContext } from './ThemeContext';
@@ -7,21 +7,22 @@ const AppContent = () => {
     const { theme, toggleTheme } = React.useContext(ThemeContext);
 
     return (
-        <div style={{ background: theme.primary }}>
-            <STLViewer url="/ControllerForge.stl" />
-            <button onClick={toggleTheme} style={{ backgroundColor: theme.secondary, color: theme.primary }}>
+        <div style={{background: theme.primary}}>
+            <button onClick={toggleTheme} style={{backgroundColor: theme.secondary, color: theme.primary}}>
                 Toggle Theme
             </button>
+            <STLViewer url="/ControllerForge.stl"/>
+
         </div>
     );
 };
 
 function App() {
-    return(
-    <ThemeProvider>
-        <AppContent />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <AppContent/>
+        </ThemeProvider>
+    );
 }
 
 export default App;
