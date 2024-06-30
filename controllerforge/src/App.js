9a7753fded9484d102b7406caebe4eab32a983ef
Jack Burnett <jackjburnett@gmail.com>
2024-06-30 12:26:10 +0100

    Added Dark mode button

--- controllerforge/src/App.js
+++ controllerforge/src/App.js
@@ -4,6 +4,7 @@ import STLViewer from './STLViewer';
 import {ThemeProvider, ThemeContext} from './ThemeContext';
 import './fonts.css';
 import InfoIcon from '@mui/icons-material/Info';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import {thirdParty} from "./colors";
 
@@ -12,21 +13,24 @@ const AppContent = () => {
 
     return (<div style={{background: theme.primary}}>
         <div className="logo" style={{color: theme.secondary}}>
-            <img src={"./logo192.png"}></img>
+            <img src={"./logo192.png"} alt={"Logo"}></img>
             ControllerForge
         </div>
+        <button style={{backgroundColor: theme.secondary, color: theme.primary, fontFamily: "Lexend Deca"}}>
+            Get Controller Parameters
+        </button>
+        <button style={{backgroundColor: theme.tertiary, color: theme.contrast, fontFamily: "Lexend Deca"}}>
+            Generate STEP
+        </button>
+        <button style={{backgroundColor: theme.tertiary, color: theme.contrast, fontFamily: "Lexend Deca"}}>
+            Generate PCB
+        </button>
+        <button style={{backgroundColor: theme.tertiary, color: theme.contrast, fontFamily: "Lexend Deca"}}>
+            Conflict Checker
+        </button>
         <button onClick={toggleTheme}
-                style={{backgroundColor: theme.secondary, color: theme.primary, fontFamily: "Lexend Deca"}}>
-            Toggle Theme
-        </button>
-        <button style={{backgroundColor: theme.tertiary, color: theme.contrast, fontFamily: "Lexend Deca"}}>
-            Button 2
-        </button>
-        <button style={{backgroundColor: theme.tertiary, color: theme.contrast, fontFamily: "Lexend Deca"}}>
-            Button 3
-        </button>
-        <button style={{backgroundColor: theme.tertiary, color: theme.contrast, fontFamily: "Lexend Deca"}}>
-            Button 4
+                style={{border: 'none', background: 'none', cursor: 'pointer'}}>
+            <DarkModeIcon style={{fontSize: 32, color: theme.contrast}}/>
         </button>
         <button style={{border: 'none', background: 'none', cursor: 'pointer'}}>
             <InfoIcon style={{fontSize: 32, color: theme.contrast}}/>
@@ -37,14 +41,14 @@ const AppContent = () => {
             color: thirdParty.orcaSlicerText,
             fontFamily: "Lexend Deca"
         }}>
-            Button 2
+            OrcaSlicer
         </button>
         <button style={{
             backgroundColor: thirdParty.kicadBackground,
             color: thirdParty.kicadText,
             fontFamily: "Lexend Deca"
         }}>
-            Button 3
+            KiCad
         </button>
         <button style={{border: 'none', background: 'none', cursor: 'pointer'}}>
             <GitHubIcon style={{fontSize: 32, color: theme.contrast}}/>
