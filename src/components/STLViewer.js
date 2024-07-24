08ea013a076da5a6c8d213497d278658f37ded8c
Jack Burnett <jackjburnett@gmail.com>
2024-07-24 01:50:05 +0100

    Update STLViewer.js

--- src/components/STLViewer.js
+++ src/components/STLViewer.js
@@ -4,7 +4,8 @@ import {OrbitControls} from '@react-three/drei';
 import {STLLoader} from 'three/examples/jsm/loaders/STLLoader';
 import * as THREE from 'three';
 import './STLViewer.css';
-import {ThemeContext} from './ThemeContext';
+import {ThemeContext} from './ThemeContext.js';
+
 
 const Model = ({url}) => {
     const geometry = useLoader(STLLoader, url);
@@ -23,25 +24,22 @@ const Model = ({url}) => {
 
     const {theme} = useContext(ThemeContext);
 
-    return (
-        <mesh geometry={geometry} ref={meshRef}>
-            <meshStandardMaterial color={theme.secondary}/>
-        </mesh>
-    );
+    return (<mesh geometry={geometry} ref={meshRef}>
+        <meshStandardMaterial color={theme.secondary}/>
+    </mesh>);
 };
 
 const STLViewer = ({url}) => {
+    // TODO: Sort lighting
     return (
         <Canvas className="stl-canvas" camera={{position: [0, 0, 200]}}>
             <ambientLight intensity={2}/>
-            <directionalLight position={[10, 10, 10]} intensity={1}/>
-            <pointLight position={[-10, -10, -10]} intensity={1}/>
+            <directionalLight position={[50, 10, 10]} intensity={1}/>
             <OrbitControls/>
-            <React.Suspense fallback={<span>Loading...</span>}>
+            <React.Suspense fallback={null}>
                 <Model url={url}/>
             </React.Suspense>
-        </Canvas>
-    );
+        </Canvas>);
 };
 
 export default STLViewer;
